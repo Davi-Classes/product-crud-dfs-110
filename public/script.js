f502f65e9f8ae3d8dc21130f6cf5399c339a7c87
Davi Gomes Lucciola <69369398+davi-lucciola@users.noreply.github.com>
2026-03-01 10:32:20 -0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,69 @@
+const produtos = [];
+
+const pMensagem = document.getElementById("mensagem");
+const produtoForm = document.getElementById("produto-form");
+const produtosTable = document.getElementById("produtos-table");
+
+function mostrarMensagem(text, type) {
+  pMensagem.innerText = text;
+
+  pMensagem.style.fontWeight = "bold";
+  pMensagem.style.color = type == "success" ? "green" : "red";
+}
+
+function adicionarProdutoTabela(produto) {
+  const tableData = produtosTable.querySelector("tbody");
+
+  const tableRow = document.createElement("tr");
+  tableData.appendChild(tableRow);
+
+  const nameCell = document.createElement("td");
+  nameCell.innerText = produto.nome;
+  tableRow.appendChild(nameCell);
+
+  const priceCell = document.createElement("td");
+  priceCell.innerText = produto.preco.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  tableRow.appendChild(priceCell);
+
+  const categoryCell = document.createElement("td");
+  categoryCell.innerText = produto.categoria;
+  tableRow.appendChild(categoryCell);
+}
+
+produtoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const produto = {
+    nome: produtoForm.nome.value.trim(),
+    preco: produtoForm.preco.value.trim(),
+    categoria: produtoForm.categoria.value,
+  };
+
+  if (produto.nome === "") {
+    mostrarMensagem("O campo nome é obrigatório.", "error");
+    return;
+  }
+
+  if (produto.preco === "") {
+    mostrarMensagem("O campo preço é obrigatório.", "error");
+    return;
+  }
+
+  produto.preco = Number(produto.preco);
+
+  if (produto.preco <= 0) {
+    mostrarMensagem("O preço deve ser maior que zero.", "error");
+    return;
+  }
+
+  produto.categoria = produto.categoria === "" ? null : produto.categoria;
+
+  produtos.push(produto);
+  adicionarProdutoTabela(produto);
+  mostrarMensagem("Produto cadastrado com sucesso.", "success");
+
+  produtoForm.reset();
+});
