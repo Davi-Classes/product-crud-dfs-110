843f4ad5cacc53ebccfe6fc6afa070680d98f6de
Davi Lucciola <davilucciola14@gmail.com>
2026-03-08 12:39:49 -0300

    feat: adicionando toast de mensagem

--- public/js/produto.view.js
+++ public/js/produto.view.js
@@ -2,11 +2,17 @@
 export const produtoForm = document.getElementById("produto-form");
 
 export function mostrarMensagem(text, type) {
-  const pMensagem = document.getElementById("mensagem");
-
-  pMensagem.innerText = text;
-  pMensagem.style.fontWeight = "bold";
-  pMensagem.style.color = type == "success" ? "green" : "red";
+  Toastify({
+    text: text,
+    duration: 3000,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hoverck
+    style: {
+      background: type === "success" ? "green" : "red",
+    },
+  }).showToast();
 }
 
 export function adicionarProdutoTabela(produto) {
